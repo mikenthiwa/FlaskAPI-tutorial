dd1bfe6a2e71a96bab0d7219d2dd3f939829e7ee
Michael Nthiwa <mike.nthiwa@gmail.com>
2018-04-12 09:06:55 +0300

    Adding JWT for authentication in login

--- api.py
+++ api.py
@@ -1,39 +1,14 @@
 from flask import Flask, jsonify, request
 from data import Books, Users
-from flask_login import LoginManager, current_user
+from flask_jwt_extended import (JWTManager, jwt_required, create_access_token, get_jwt_identity)
 
 app = Flask(__name__)
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return Users.get_id(user_id)
-
-
-@app.route('/api/auth/login', methods=['POST'])
-def login():
-    books = Books()
-    user = Users()
-    if current_user.is_authenticated:
-        return jsonify(books.get_all_books())
-    user_cred = request.get_json()
-    all_users = user.all_users()
-    username_password_db = {}
-
-    for u_name in all_users:
-        username_password_db[u_name] = all_users[u_name][1]
-
-    for u_name in user_cred:
-        if u_name in username_password_db:
-            if user_cred[u_name] == username_password_db[u_name]:
-                return jsonify(books.get_all_books())
-            else:
-                return 'Wrong password'
-        else:
-            return 'Invalid username. Register ?'
+app.config['JWT_SECRET_KEY'] = 'super-secret'
+jwt = JWTManager(app)
+book = Books()
+user = Users()
+users = user.all_users()
+books = book.get_all_books()
 
 
 @app.route('/', methods=['GET'])
@@ -41,65 +16,8 @@ def index():
     return jsonify({"message": "it works"})
 
 
-@app.route('/api/books', methods=['GET'])
-def all_books():
-    books = Books()
-    return jsonify(books.get_all_books())
-
-
-@app.route('/api/books/<int:book_id>', methods=['GET'])
-def specific_book(book_id):
-    book = Books()
-    return jsonify(book.get_a_book(book_id))
-
-
-@app.route('/api/books/<int:book_id>', methods=['PUT'])
-def modify_book_info(book_id):
-    book = Books()
-    req_data = request.get_json()
-    book_info = ""
-
-    for values in req_data.values():
-        for value in values.values():
-            book_info = value
-
-    book.update_book_info(book_id, book_info)
-    return jsonify(book.get_all_books())
-
-
-@app.route('/api/books/<book_id>', methods=['DELETE'])
-def remove_a_book(book_id):
-    book = Books()
-    try:
-        book.delete_book(book_id)
-    except KeyError:
-        print(book_id.isalpha())
-    return jsonify(book.get_all_books())
-
-
-@app.route('/api/books', methods=['GET', 'POST'])
-def add_new_book():
-    new_book = Books()
-
-    if request.method == 'POST':
-        req_data = request.get_json()
-        keys = ""
-        values = ""
-
-        for key in req_data.keys():
-            keys = key
-
-        for data in req_data.values():
-            for value in data.values():
-                values = value
-
-        new_book.add_book(int(keys), values)
-    return jsonify(new_book.get_all_books())
-
-
 @app.route('/api/auth/register', methods=['POST'])
 def new_user():
-    user = Users()
     req_data = request.get_json()
     username = ""
     email = ""
@@ -116,12 +34,96 @@ def new_user():
     return "Username : {}\nPassword : {}\n\nPlease Log In".format(username, password)
 
 
-@app.route('/api/auth/reset-password', methods=['POST'])
-def reset_password():
-    users = Users()
+@app.route('/api/auth/login', methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing json in request"}), 400
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+
+    if not username and not password:
+        return jsonify({"msg": 'username and password are empty'})
+    elif not username:
+        return jsonify({"msg": 'username is empty'})
+    elif not password:
+        return jsonify({"msg": 'password is empty'})
+    else:
+        if username not in users:
+            return jsonify({'msg': "Invalid user"})
+        elif password != users[username][1]:
+            return jsonify({"msg": 'wrong password'})
+
+    access_token = create_access_token(identity=username)
+
+    return jsonify(access_token=access_token), 200
+
+
+@app.route('/api/v1/books', methods=['GET'])
+def all_books():
+    return jsonify(book.get_all_books())
+
+
+@app.route('/api/v1/users/books/<int:book_id>', methods=['GET'])
+@jwt_required
+def borrow(book_id):
+    current_user = get_jwt_identity()
+    print(type(book_id))
+    if current_user in users:
+        if book_id in books:
+            print('available')
+    return jsonify(logged_in_as=current_user), 200
+
+
+@app.route('/api/v1/books/<int:book_id>', methods=['GET'])
+def specific_book(book_id):
+    return jsonify(book.get_a_book(book_id))
+
+
+@app.route('/api/v1/books/<int:book_id>', methods=['PUT'])
+def modify_book_info(book_id):
+    req_data = request.get_json()
+    book_info = ""
+
+    for values in req_data.values():
+        for value in values.values():
+            book_info = value
+
+    book.update_book_info(book_id, book_info)
+    return jsonify(book.get_all_books())
+
+
+@app.route('/api/books/<book_id>', methods=['DELETE'])
+def remove_a_book(book_id):
+
+    try:
+        book.delete_book(book_id)
+    except KeyError:
+        print(book_id.isalpha())
+    return jsonify(book.get_all_books())
+
+
+@app.route('/api/v1/books', methods=['POST'])
+def add_new_book():
 
     req_data = request.get_json()
-    user_data = users.all_users()
+    book_id = ""
+    author = ""
+    title = ""
+
+    for key in req_data:
+        book_id = key
+        for key_val in req_data[key]:
+            title = key_val
+            author = req_data[key][key_val]
+
+    book.add_book(int(book_id), title, author)
+    return "You have added\nBook_Id: {}\nTitle: {}\nAuthor: {}".format(book_id, title, author)
+
+
+@app.route('/api/auth/reset-password', methods=['POST'])
+def reset_password():
+    req_data = request.get_json()
+    user_data = user.all_users()
     e_mail = ""
     password = ""
     username = ""
@@ -132,7 +134,7 @@ def reset_password():
     for u_name in user_data:
         username = u_name
         if user_data[username][0] == e_mail:
-            users.change_password(username, password)
+            user.change_password(username, password)
 
     return 'Username : {}\nNew password : {}'.format(username, password)
 
